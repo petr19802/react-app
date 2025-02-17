c5d6a34c1d23393a20a6bb7f9a33fbf88a23ec56
Сергей Блохин <sblohin@cloudfactory.ru>
2025-02-17 14:29:55 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000, // Здесь указываем нужный порт
+    watch:{
+      usePolling:true
+    },
+    host:true,
+    strictPort:true
+  }
+})
